c961c403a8ee483daa61466694947da8adcea7c5
alina <alina.abraham@stud.hs-flensburg.de>
2025-02-24 19:48:08 +0100

    minor refactoring

--- courses.py
+++ courses.py
@@ -1,13 +1,19 @@
 # enrolment urls of all courses to enroll to
-my_courses = [
-    "https://elearning.hs-flensburg.de/dispatch.php/course/details/index/14bc021c02a7db4d13e006c28494fbed",
-    "https://elearning.hs-flensburg.de/dispatch.php/course/details/index/4c9d168f18c1a4f91cf6fa1d8cd154d8",
-    "https://elearning.hs-flensburg.de/dispatch.php/course/details/index/878ed43e7280274c144a21948cd36cc7"
-]
-
-# course names in same order as links
-my_course_names = [
-    "Theoretische Informatik",
-    "TOP-Woche",
-    "Web-Technologien"
-]
+my_courses = {
+    "V-Software-Engineering-1": "",
+    "L-Software Engineering-1": "",
+    "V-Theoretische-Informatik": "https://elearning.hs-flensburg.de/dispatch.php/course/details/index/14bc021c02a7db4d13e006c28494fbed",
+    "Ü-Theoretische-Informatik": "https://elearning.hs-flensburg.de/dispatch.php/course/details/index/13fd1e6cbe0c6227d2751b482756a4c7",
+    "V-Mobile-Computing": "",
+    "L-Mobile-Computing": "",
+    "V-3D-Engine-Technology": "",
+    "L-3D-Engine-Technology": "",
+    "V-Web-Technologien": "https://elearning.hs-flensburg.de/dispatch.php/course/details/index/878ed43e7280274c144a21948cd36cc7",
+    "L-Web-Technologien": "",
+    "V-Mathe": "",
+    "Ü-Mathe": "",
+    "Grundlagen-der-IT-Security": "",
+    "V-Datenbanken": "",
+    "L-Datenbanken": "",
+    "Betriebswirtschaftslehre": "",
+}
--- main.py
+++ main.py
@@ -1,50 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
 from login import do_login
-from courses import my_courses, my_course_names
+from courses import my_courses
 
 session = requests.Session()
 
 do_login(session)
 
-course_count = 0
+for my_course, my_url in my_courses.items():
+    print("")
+    print(f"{my_course}:")
+    try:
+        course_response = session.get(my_url)
+        print(f"✓ Url")
+        course_soup = BeautifulSoup(course_response.text, 'html.parser')
 
-for my_course in my_courses:
-    course_url = my_course
-    course_response = session.get(course_url)
-    course_soup = BeautifulSoup(course_response.text, 'html.parser')
-
-    print(my_course_names[course_count])
-    course_count = course_count + 1
-
-    enrolment_link = course_soup.find('a', {'data-dialog': 'size=big'})
-    if enrolment_link:
-        enrolment_url = enrolment_link['href']
-        print("✓ Link")
-    else:
-        print("✗ Link")
-
-    if enrolment_url:
-        security_token = course_soup.find('input', {'name': 'security_token'})['value']
-        enrolment_data = {
-            'security_token': security_token,
-            'apply': '1',
-        }
-
-        enrolment_response = session.post(enrolment_url, data=enrolment_data)
-
-        # check for success
-        if enrolment_response.status_code == 200:
-            soup = BeautifulSoup(enrolment_response.text, 'html.parser')
-            page_text = soup.get_text()
-            time_out_message = "nicht innerhalb des Anmeldezeitraums"
-            if time_out_message in page_text:
-                print("✗ Fehler: außerhalb des Anmeldezeitraums")
-            else:
-                print("✓ Anmeldung erfolgreich")
+        enrolment_link = course_soup.find('a', {'data-dialog': 'size=big'})
+        if enrolment_link:
+            enrolment_url = enrolment_link['href']
+            print("✓ Link")
         else:
-            print("✗ Fehler: Netzwerkfehler")
-    else:
-        print("✗ Fehler: keine gültige URL gefunden")
+            print("✗ Link")
+
+        if enrolment_url:
+            security_token = course_soup.find('input', {'name': 'security_token'})['value']
+            enrolment_data = {
+                'security_token': security_token,
+                'apply': '1',
+            }
+
+            enrolment_response = session.post(enrolment_url, data=enrolment_data)
+
+            # check for success
+            if enrolment_response.status_code == 200:
+                soup = BeautifulSoup(enrolment_response.text, 'html.parser')
+                page_text = soup.get_text()
+                time_out_message = "nicht innerhalb des Anmeldezeitraums"
+                if time_out_message in page_text:
+                    print("✗ Fehler: außerhalb des Anmeldezeitraums")
+                else:
+                    print("✓ Anmeldung erfolgreich")
+            else:
+                print("✗ Fehler: Netzwerkfehler")
+        else:
+            print("✗ Fehler: keine gültige URL gefunden")
+
+    except requests.exceptions.RequestException as e:
+        print(f"✗ Url")
+
+    except Exception as e:
+        print(f"✗ unbekannter Fehler")
 
 session.close()
